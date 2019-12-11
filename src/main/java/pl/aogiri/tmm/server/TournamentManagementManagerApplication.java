d4e9f6fa75ba19aa6907fe121fa94921ff91fe03
Kaneki #Senapi <v.kaneki.senpai@gmail.com>
2019-12-11 01:09:19 +0100

    Init project

---
+++ src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.aogiri.tmm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentManagementManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentManagementManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/aogiri/tmm/server/TournamentManagementManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aogiri.tmm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentManagementManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
