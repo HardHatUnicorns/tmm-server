ca53b122de747338bf13066a3d5bfea4d64dcfa4
KanekiSenapi <v.kaneki.senpai@gmail.com>
2020-03-07 00:14:58 +0100

    start

--- src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
+++ src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
@@ -2,9 +2,16 @@ package pl.aogiri.tmm.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TournamentManagementManagerApplication {
+public class TournamentManagementManagerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(TournamentManagementManagerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TournamentManagementManagerApplication.class, args);
