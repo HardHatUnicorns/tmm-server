6e0921614c2053d7d322b05208c099581b1ab3a1
Kaneki #Senapi <43042539+KanekiSenapi@users.noreply.github.com>
2020-03-07 22:41:32 +0100

    TMM-101 #Tomcat to wildfly  (#3)
    * change java version to 11
    
    * start
    
    * work
    
    * wildfly
    
    * Update application.properties
    
    Co-authored-by: KanekiSenapi <v.kaneki.senpai@gmail.com>

--- src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
+++ src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
@@ -2,9 +2,20 @@ package pl.aogiri.tmm.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class TournamentManagementManagerApplication {
+public class TournamentManagementManagerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(TournamentManagementManagerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TournamentManagementManagerApplication.class, args);
