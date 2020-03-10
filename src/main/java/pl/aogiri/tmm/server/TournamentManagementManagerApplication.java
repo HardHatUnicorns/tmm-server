ec43abc0bf47c63778c056c506544d4664e53718
KanekiSenapi <v.kaneki.senpai@gmail.com>
2020-03-10 23:02:09 +0100

    swagger

--- src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
+++ src/main/java/pl/aogiri/tmm/server/TournamentManagementManagerApplication.java
@@ -2,8 +2,10 @@ package pl.aogiri.tmm.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TournamentManagementManagerApplication {
 
     public static void main(String[] args) {
