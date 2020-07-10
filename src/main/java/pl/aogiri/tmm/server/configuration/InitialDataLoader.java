d0a78066cefdf8cf1076caa03a43d8259fb3ebc1
Kaneki #Senapi <43042539+KanekiSenapi@users.noreply.github.com>
2020-07-11 01:09:51 +0200

    2 profiles + default dev + basic user (#16)
    * 2 profiles + default dev + basic user
    
    * hide sql on prod
    
    Co-authored-by: KanekiSenapi <v.kaneki.senpai@gmail.com>

--- src/main/java/pl/aogiri/tmm/server/configuration/InitialDataLoader.java
+++ src/main/java/pl/aogiri/tmm/server/configuration/InitialDataLoader.java
@@ -2,23 +2,21 @@ package pl.aogiri.tmm.server.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import pl.aogiri.tmm.server.dao.implementation.PrivilegeDAO;
 import pl.aogiri.tmm.server.dao.implementation.RoleDAO;
 import pl.aogiri.tmm.server.dao.implementation.UserDAO;
-import pl.aogiri.tmm.server.entity.implementation.PrivilegeEntity;
-import pl.aogiri.tmm.server.entity.implementation.RoleEntity;
+import pl.aogiri.tmm.server.entity.implementation.UserEntity;
 import pl.aogiri.tmm.server.util.PasswordEncoder;
-
-import java.util.Collection;
 
 @Component
 @Transactional
 public class InitialDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    boolean alreadySetup = false;
+    static boolean alreadySetup = false;
 
     private PrivilegeDAO privilegeDAO;
     private RoleDAO roleDAO;
@@ -33,53 +31,21 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Profile("dev")
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-//        if (alreadySetup)
-//            return;
-//        PrivilegeEntity readPrivilege
-//                = createPrivilegeIfNotFound("ADMIN_PRIVILEGE");
-//
-//        List<PrivilegeEntity> adminPrivileges = Collections.singletonList(
-//                readPrivilege);
-//        createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
-//
-//        RoleEntity adminRole = roleDAO.findByName("ROLE_ADMIN");
-//        UserEntity user = new UserEntity();
-//        user.setLogin("root");
-//        user.setPassword(passwordEncoder.encode("root"));
-//        user.setRoles(Collections.singletonList(adminRole));
-//        user.setEmail("root@root.pl");
-//        userDAO.save(user);
-//
-//        alreadySetup = true;
+        if (alreadySetup)
+            return;
+
+        UserEntity user = new UserEntity();
+        user.setLogin("root");
+        user.setPassword(passwordEncoder.encode("root"));
+        user.setEmail("root@root.pl");
+        userDAO.save(user);
+        user.setEnabled(true);
+        userDAO.save(user);
+
+        alreadySetup = true;
     }
 
-
-
-    @Transactional
-    public PrivilegeEntity createPrivilegeIfNotFound(String name) {
-
-        PrivilegeEntity privilege = privilegeDAO.findByName(name);
-        if (privilege == null) {
-            privilege = new PrivilegeEntity();
-            privilege.setName(name);
-            privilegeDAO.save(privilege);
-        }
-        return privilege;
-    }
-
-    @Transactional
-    public RoleEntity createRoleIfNotFound(
-            String name, Collection<PrivilegeEntity> privileges) {
-
-        RoleEntity role = roleDAO.findByName(name);
-        if (role == null) {
-            role = new RoleEntity();
-            role.setName(name);
-            role.setPrivileges(privileges);
-            roleDAO.save(role);
-        }
-        return role;
-    }
 }
